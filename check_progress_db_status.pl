86dc3521cc3564c36ca144b15605910d66d57959
maheanuu <maheanuu@gmail.com>
2023-08-26 07:44:54 -1000

    Small changes in the code

--- check_progress_db_status.pl
+++ check_progress_db_status.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-my $REMOTE_HOST = "192.168.203.109";
-my $FILE = "/fenua/scripts/status.out";
-# Utiliser SSH pour récupérer le contenu du fichier de la machine distante
+my $REMOTE_HOST = "YOUR_DB_IP_ADRESS";
+my $FILE = "/your/script/status.out";
+# Use SSH to get status out from progress server
 my $status = `/usr/bin/ssh $REMOTE_HOST "cat $FILE" 2>&1`;
 chomp $status;
 if ($status eq "1") {
-    print "OK: La base de donnée FENUA est active\n";
+    print "OK: Database is active\n";
     exit 0;
 }
 elsif ($status eq "0") {
-    print "WARNING: La base de donnée FENUA est inactive\n";
+    print "WARNING: Database is inactive\n";
     exit 1;
 }
 else {
